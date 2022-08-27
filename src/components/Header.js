13c30f3118254483896e6c85175a1f715bcce05a
rakeshwasnik <rbwasnik99@gmail.com>
2022-08-27 21:32:36 +0530

    .

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Header.css'
+import punkLogo from '../assets/header/cryptopunk-logo.png'
+import searchIcon from '../assets/header/search.png'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        
+        <div className='logoContainer'>
+            <img src={punkLogo} className='punkLogo' alt='' />
+        </div>
+
+        <div className='searchBar'>
+            <div className='searchIconContainer'>
+                <img src={searchIcon} />
+            </div>
+            <input
+              className='searchInput'
+              placeholder='Collection, Item or User... '
+            />
+        </div>
+    </div>
+  )
+}
+
+export default Header
