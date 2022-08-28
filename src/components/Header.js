49927f91c4f0898c620c643353c83ae7f8da55b6
rakeshwasnik <rbwasnik99@gmail.com>
2022-08-28 22:18:04 +0530

    .

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Header.css'
 import punkLogo from '../assets/header/cryptopunk-logo.png'
 import searchIcon from '../assets/header/search.png'
+import darkModeIcon from '../assets/header/theme-switch.png'
 
 const Header = () => {
   return (
@@ -13,13 +14,28 @@ const Header = () => {
 
         <div className='searchBar'>
             <div className='searchIconContainer'>
-                <img src={searchIcon} />
+                <img src={searchIcon} alt=''/>
             </div>
             <input
               className='searchInput'
               placeholder='Collection, Item or User... '
             />
         </div>
+
+        <div className='headerItems'>
+          <p>Drops</p>
+          <p>Marketplace</p>
+          <p>Create</p>
+        </div>
+
+        <div className='headerActions'>
+          <div className='darkModeContainer'>
+            <img src={darkModeIcon} alt='' />
+          </div>
+        </div>
+
+        <div className='loginButton'>GET IN</div>
+
     </div>
   )
 }
